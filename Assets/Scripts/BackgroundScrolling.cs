145ece2e00623b46ed895109008bdad51e3cd181
Enlik Tjioe <enlik.stefanus@gmail.com>
2017-01-29 09:06:52 +1100

    Implement 2D Infinite Scrolling Background, reference: https://www.youtube.com/watch?v=HrDxnMI7pCc

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+	public float scrollingSpeed = 0.5f;
+	void Start () {
+		
+	}
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * scrollingSpeed, 0);
+
+		this.gameObject.GetComponent <MeshRenderer> ().material.mainTextureOffset = offset;
+	}
+}
